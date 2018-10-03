c0f4ee322150039d9f4bea5473e0de2a22e32de3
iamaturtle113 <i_am_a_turtle_yh@yahoo.co.jp>
2018-10-03 17:11:09 +0900

    First thread program

---
+++ Thread1.java
@@ -0,0 +1,62 @@
+/* Practic Thread class.(java.lang.Thread)
+ * Especially implement Runnable interface and override run()
+ * 
+ */
+public class Thread1 
+{
+	
+	public static class ImpleRunnable implements Runnable
+	{
+
+		@Override
+		public void run() {
+			for(int i=0;i<10;i++)
+			{
+				System.out.println("Hey, What's up?!");
+				try
+				{
+					Thread.sleep(1000);
+				}
+						catch(InterruptedException e) 
+						{
+							e.printStackTrace();
+						}
+			}
+		}
+	}
+	
+	
+	public static class ImpleRunnable2 implements Runnable
+	{
+
+		@Override
+		public void run() {
+			for(int i=0;i<10;i++)
+			{
+			System.out.println("Are you sure??");
+				try
+				{
+					Thread.sleep(2000);
+					}
+						catch(InterruptedException e) 
+						{
+							e.printStackTrace();
+						}
+				}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Runnable threadJob=new ImpleRunnable();
+		Thread myThread=new Thread(threadJob);
+		myThread.start();
+		
+		Runnable threadJob2=new ImpleRunnable2();
+		Thread myThread2=new Thread(threadJob2);
+		myThread2.start();
+		
+	}
+}
+
+//First 2018-10-03-Wed
